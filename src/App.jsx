c43e704681db8d98ebed84f927611ef95a324b01
Mehandi <mehandixaidi786@gmail.com>
2024-12-11 15:41:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './index.css'
+import ProductCard from './component/ProductCard'
+
+const App = () => {
+  return (
+    <div>
+      <ProductCard />
+    </div>
+  )
+}
+
+export default App
